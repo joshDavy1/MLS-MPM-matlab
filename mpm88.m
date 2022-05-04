a3bd12c4afa92a59d9ce89eeab608609fd90fe19
Josh Davy <joshdavy13@gmail.com>
2022-05-04 01:14:56 +0100

    Refactored Code

--- mpm88.m
+++ mpm88.m
@@ -21,12 +21,12 @@ Y = 2;
 
 %init
 for i=1:n_particles
-    x(:,i) = [rand()*0.4+0.2,rand()*0.4+0.2];
+    x(:,i) = [rand()*0.4 + 0.2,rand()*0.4 + 0.2];
     v(:,i) = [0 -1];
     J(i)  =  1;
 end
 
-filename = "mpm88.png"
+filename = "mpm88.png";
 for iter=1:5000
     iter
     % Plot
@@ -34,28 +34,28 @@ for iter=1:5000
     ylim([0 1]);
     xlim([0 1]);
     drawnow
-    frame = getframe(1);
-    im = frame2im(frame);
-    [imind,cm] = rgb2ind(im,256);
     if mod(iter,25) == 1
+        frame = getframe(1);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
       imwrite(imind,cm,"gif/"+string(fix(iter/25))+"-"+filename,'png');
     end
     grid_v = zeros([2 n_grid n_grid]);
     grid_m = zeros([n_grid n_grid]);
     for p=1:n_particles
         Xp = x(:,p) / dx;
-        base = fix(Xp - 0.5);
-        fx = Xp - base ;
-        w = [0.5*(1.5-fx).^2,0.75-(fx-1).^2,0.5*(fx-0.5).^2];
-        stress = -dt*4*E*p_vol*(J(p)-1) / dx^2;
+        base = floor(Xp - 0.5);
+        fx = Xp - base;
+        w = [0.5*(1.5 - fx).^2,0.75 - (fx - 1).^2,0.5*(fx - 0.5).^2];
+        stress = -dt*4*E*p_vol*(J(p) - 1) / dx^2;
         affine = [stress 0; 0 stress] + p_mass*C(:,:,p);
         for i=1:3 
             for j=1:3
-                offset = [i;j]-1; % Matlab indexes from 1
+                offset = [i;j] - 1; % Matlab indexes from 1
                 dpos = (offset-fx)*dx;
-                weight = w(X,i) * w(Y,j);
+                weight = w(X,i)*w(Y,j);
                 index = base+offset;
-                grid_v(:,index(X),index(Y)) = grid_v(:,index(X),index(Y)) + weight * (p_mass * v(:,p) + affine * dpos);
+                grid_v(:,index(X),index(Y)) = grid_v(:,index(X),index(Y)) + weight*(p_mass*v(:,p) + affine* dpos);
                 grid_m(index(X),index(Y)) = grid_m(index(X),index(Y)) + weight*p_mass;
             end
         end
@@ -82,16 +82,16 @@ for iter=1:5000
     end
     for p=1:n_particles
         Xp = x(:,p) / dx;
-        base = fix(Xp - 0.5);
+        base = floor(Xp - 0.5);
         fx = Xp - base;
-        w = [0.5*(1.5-fx).^2,0.75-(fx-1).^2,0.5*(fx-0.5).^2];
+        w = [0.5*(1.5 - fx).^2,0.75 - (fx - 1).^2,0.5*(fx - 0.5).^2];
         new_V  = zeros([2,1]);
         new_C = zeros([2,2]);
         for i=1:3
             for j=1:3
-                offset = [i;j]-1;
-                dpos = (offset-fx)*dx;
-                weight = w(X,i) * w(Y,j);
+                offset = [i;j] - 1;
+                dpos = (offset - fx)*dx;
+                weight = w(X,i)*w(Y,j);
                 index = base + offset;
                 g_v = grid_v(:,index(X),index(Y));
                 new_V = new_V + weight*g_v;
@@ -99,8 +99,8 @@ for iter=1:5000
             end
         end
         v(:,p) = new_V;
-        x(:,p) = x(:,p) + v(:,p) * dt;
-        J(p) = J(p) * (1+dt*trace(new_C));
+        x(:,p) = x(:,p) + v(:,p)*dt;
+        J(p) = J(p)*(1+dt*trace(new_C));
         C(:,:,p) = new_C;
     end
 end
